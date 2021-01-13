385eb6862a64b64fa5cc9cdcccc3523425a7a2d9
Weslley Fratini <weeslleey354@gmail.com>
2021-01-12 21:28:35 -0300

    refactor test

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,16 +1,25 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
 
-export class CurrenciesService {
-    async getCurrency(currency: string): Promise<any> { }
-}
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CurrenciesService } from '../currencies/currencies.service';
+import { ExchangeInputDto } from './dto/exchange-input.dto';
+import { ExchangeType } from './types/exchange.type';
+
 @Injectable()
 export class ExchangeService {
     constructor(private currenciesService: CurrenciesService) { }
-    async convertAmount({ from, to, amount }): Promise<any> {
+
+    async convertAmount({ from, to, amount }: ExchangeInputDto): Promise<ExchangeType> {
         if (!from || !to || !amount) {
-            throw new BadRequestException()
+            throw new BadRequestException();
         }
 
-        const currencyFrom = this.currenciesService.getCurrency(from)
+        try {
+            const currencyFrom = await this.currenciesService.getCurrency(from);
+            const currencyTo = await this.currenciesService.getCurrency(to);
+
+            return { amount: (currencyFrom.value / currencyTo.value) * amount };
+        } catch (error) {
+            throw new Error(error);
+        }
     }
 }
