96a6e60d957f25e59b75593d05dbb9fca8a57462
Weslley Fratini <weeslleey354@gmail.com>
2021-01-12 21:18:59 -0300

    terceiro teste

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,33 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ExchangeService } from './exchange.service';
+import { CurrenciesService, ExchangeService } from './exchange.service';
 import { BadRequestException } from '@nestjs/common';
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
+  let currenciesService: CurrenciesService;
 
   beforeEach(async () => {
+    const currencyServiceMock = { getCurrency: jest.fn }
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
-    }).compile();
+      providers: [
+        ExchangeService,
+        { provide: CurrenciesService, useFactory: () => currencyServiceMock };
+      ],
+  }).compile();
 
-    service = module.get<ExchangeService>(ExchangeService);
+  service = module.get<ExchangeService>(ExchangeService);
+  currenciesService = module.get<CurrenciesService>(CurrenciesService);
+});
+
+it('should be defined', () => {
+  expect(service).toBeDefined();
+});
+
+describe('ConvertAmount()', () => {
+  it('should be throw if called with invalid params', async () => {
+    await expect(
+      service.convertAmount({ from: '', to: '', amount: 0 }),
+    ).rejects.toThrow(new BadRequestException());
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be not throw if called with valid params', async () => {
+    await expect(
+      service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 })
+    ).resolves.not.toThrow();
   });
 
-  describe('ConvertAmount()', () => {
-    it('should be throw if called with invalid params', async () => {
-      await expect(
-        service.convertAmount({ from: '', to: '', amount: 0 }),
-      ).rejects.toThrow(new BadRequestException());
-    });
+  it('should be called getCurrency twice', async () => {
+    await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 })
+    await expect(currenciesService.getCurrency().toBeCalledTimes(2));
 
-    it('should be not throw if called with valid params', async () => {
-      await expect(
-        service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 })
-      ).resolves.not.toThrow();
-    });
+
   });
+});
 }); 
