e8cbbf1787d73645dcdac75ea5e8f157e2cfb742
Weslley Fratini <weeslleey354@gmail.com>
2021-01-14 20:53:05 -0300

    criação do teste should be not throw if repository returns

--- src/currencies/currencies.service.spec.ts
+++ src/currencies/currencies.service.spec.ts
@@ -1,12 +1,13 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { CurrenciesService } from './currencies.service';
+import { CurrenciesRepository, CurrenciesService } from './currencies.service';
 
 describe('CurrenciesService', () => {
   let service: CurrenciesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CurrenciesService],
+      providers: [CurrenciesService, CurrenciesRepository],
     }).compile();
 
     service = module.get<CurrenciesService>(CurrenciesService);
@@ -14,5 +15,11 @@ describe('CurrenciesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getCurrency()', () => {
+    it('should be not throw if repository returns', async () => {
+      await expect(service.getCurrency('USD')).resolves.not.toThrow();
+    });
   });
 });
